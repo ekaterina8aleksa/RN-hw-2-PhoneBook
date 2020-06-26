8067990dc17f8f9643a54a1a40208c1e98619037
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-06-27 02:42:22 +0300

    Updates

---
+++ precache-manifest.d007c8dc972ca57b001026cc88ac7009.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "569f7f5ed3c12db0fa2e03579f56d288",
+    "url": "/RN-hw-2-PhoneBook/index.html"
+  },
+  {
+    "revision": "19cad85fddae411a459f",
+    "url": "/RN-hw-2-PhoneBook/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "87f2d9407b35615a628d",
+    "url": "/RN-hw-2-PhoneBook/static/js/2.c884039c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/RN-hw-2-PhoneBook/static/js/2.c884039c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "19cad85fddae411a459f",
+    "url": "/RN-hw-2-PhoneBook/static/js/main.40c25a7c.chunk.js"
+  },
+  {
+    "revision": "8a38ff7b8d69e1f81548",
+    "url": "/RN-hw-2-PhoneBook/static/js/runtime-main.da67122e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/RN-hw-2-PhoneBook/static/media/logo.5d5d9eef.svg"
+  }
+]);
