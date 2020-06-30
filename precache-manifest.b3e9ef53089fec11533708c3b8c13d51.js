34eabc3257453a4cb9069d46d85314b0730b3099
ekaterina8aleksa <ekaterina.palad87@gmail.com>
2020-06-30 03:02:42 +0300

    Updates

---
+++ precache-manifest.b3e9ef53089fec11533708c3b8c13d51.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2c8b20e59422641e9fb211d0e4ed344a",
+    "url": "/RN-hw-2-PhoneBook/index.html"
+  },
+  {
+    "revision": "b99a2d1860b01e6f1c03",
+    "url": "/RN-hw-2-PhoneBook/static/css/2.d34346ea.chunk.css"
+  },
+  {
+    "revision": "01a8219ac6615a5cc1b5",
+    "url": "/RN-hw-2-PhoneBook/static/css/main.00017852.chunk.css"
+  },
+  {
+    "revision": "b99a2d1860b01e6f1c03",
+    "url": "/RN-hw-2-PhoneBook/static/js/2.6494ba40.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/RN-hw-2-PhoneBook/static/js/2.6494ba40.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "01a8219ac6615a5cc1b5",
+    "url": "/RN-hw-2-PhoneBook/static/js/main.9e0fed8e.chunk.js"
+  },
+  {
+    "revision": "8a38ff7b8d69e1f81548",
+    "url": "/RN-hw-2-PhoneBook/static/js/runtime-main.da67122e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/RN-hw-2-PhoneBook/precache-manifest.a095650b3d70201ce54f299c89794483.js"
+  "/RN-hw-2-PhoneBook/precache-manifest.b3e9ef53089fec11533708c3b8c13d51.js"
 );
 
 self.addEventListener('message', (event) => {
